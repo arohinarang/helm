bdd157446c70bdcd2b36e84b208234f3a7f5db33
viram shah <viram.shah@quantiphi.com>
2022-06-22 18:08:47 +0530

    changes

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,14 +4,9 @@ terraform {
       source  = "hashicorp/helm"
       version = "~> 2.0.1"
     }
-    # kubernetes = {
-    #   source  = "hashicorp/kubernetes"
-    #   version = "~> 2.0.1"
-    # }
     google = {
       source  = "hashicorp/google"
       version = "4.25.0"
     }
   }
-#   required_version = "~> 0.14"
 }
