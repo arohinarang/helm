840988dc5e6330765716232993e1a32b7a9af150
viram shah <viram.shah@quantiphi.com>
2022-06-23 17:30:04 +0530

    webapp chart

--- terraform/helm_release.tf
+++ terraform/helm_release.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "nginx_ingress" {
-  name       = "webapp-helm-chart"
-#   repository = "https://charts.bitnami.com/bitnami"
-  chart      = "../chart/mychart"
+  name = "webapp-helm-chart"
+  #   repository = "https://charts.bitnami.com/bitnami"
+  chart = "../chart/mychart"
 }
