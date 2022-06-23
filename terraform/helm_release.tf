efc1d324b259a5f2ea97210354c04908195091b9
viram shah <viram.shah@quantiphi.com>
2022-06-23 17:17:21 +0530

    nginx chart

--- terraform/helm_release.tf
+++ terraform/helm_release.tf
@@ -1,5 +1,5 @@
 resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress-controller"
   repository = "https://charts.bitnami.com/bitnami"
-  chart      = "nginx-ingress-controller"
+  chart      = "bitnami/nginx"
 }
